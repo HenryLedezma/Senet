3983235c9ce51638dfb1c5ab44cc0e5cfbff64bc
JOSUE.RETANARODRIGUEZ <josue.retanarodriguez@ucr.ac.cr>
2022-11-03 01:44:48 -0600

    Agrega controlador del juego

--- src/main.cpp
+++ src/main.cpp
@@ -5,40 +5,55 @@
 #include "Tablero.h"
 #include "Mediador.h"
 
+/*Definicón Códigos*/
+// Flag: En caso de el turno sea del jugador 1
+#define TURN_PLAYER1 99
+// Flag: En caso de el turno sea del jugador 2
+#define TURN_PLAYER2 100
+
+
 using namespace std;
 
-// Para testing, print del estado del juego
+// Para testing, print del estado del juego en consola
 void printGame(Tablero tablero);
+
+// Selecciona ficha a mover
+int selectFicha();
 
 int main() {
   // Se crea y setea el tablero
   Tablero tablero = Tablero();
   Mediador mediador;
-
   mediador.setupTablero(tablero);
-  printGame(tablero);
+  bool winDetected = false;         // Bool si alguien ha ganado
+  int dado_result = 0;              // Resultado del dado
+  int current_turn = TURN_PLAYER1;  // Turno actual, default player 1
+  int who_won = 0;                  // Quién ganó la partida
+  bool continue_turn = true;        // Indica si el jugador debe seguir jugando
 
-
-  // Se prueba realizar un movimiento
-  mediador.realizarMovimiento(tablero.tablero[9].ficha, 2, tablero);
-  printGame(tablero);
-
-  // Se prueba realizar un movimiento
-  mediador.realizarMovimiento(tablero.tablero[11].ficha, 28, tablero);
-  printGame(tablero);
-
-  mediador.checkWin();
-
-  /* TESTING asignacion de ficha a casillas */
-  printf("--------------------------------------\n");
-  for (int i = 0; i < tablero.tablero.size(); i++) {
-    if (tablero.tablero[i].ocupada == 1) {
-      cout << "Soy la casilla: " << i << " y tengo a la ficha en la posc ";
-      cout << tablero.tablero[i].ficha.getPosicion() << " \n"; 
+  while (!winDetected) {
+    // Revisa si alguien ha ganado
+    if (mediador.checkWin() == PLAYER1_WINS) {
+      winDetected = true;
+      who_won = PLAYER1_WINS;
+      break;
     }
+    if (mediador.checkWin() == PLAYER2_WINS) {
+      winDetected = true;
+      who_won = PLAYER2_WINS;
+      break;
+    }
+    // Turno como tal del jugador
+    do {
+      dado_result = rollDice();
+      int poscCasilla = selectFicha();
+      mediador.realizarMovimiento(tablero.tablero[poscCasilla - 1].ficha, dado_result, tablero);
+      printGame(tablero); // borrar, testing en consola
+      continue_turn = mediador.sigoJugando(dado_result);
+    } while (continue_turn);
+    // Intercambio de turnos
+    (current_turn == TURN_PLAYER1) ? current_turn = TURN_PLAYER2 :  current_turn = TURN_PLAYER1;
   }
-  
-  
 }
 
 void printGame(Tablero tablero) {
@@ -47,7 +62,7 @@ void printGame(Tablero tablero) {
     if (tablero.tablero[i].ocupada == 1) {
       cout << tablero.tablero[i].ficha.getJugadorID() << " "; 
     } else {
-      printf("Vacio ");
+      printf("X ");
     }
     
     if (i == 9) {printf("\n");}
@@ -55,4 +70,10 @@ void printGame(Tablero tablero) {
     if (i == 29) {printf("\n");}
   }
   printf("--------------------------------------\n");
+}
+
+int selectFicha() {
+  int poscCasilla = 0;
+  cout << "Type num. casilla donde esta la ficha a mover: "; // Type a number and press enter
+  cin >> poscCasilla; // Get user input from the keyboard
 }
